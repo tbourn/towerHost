67254ae503a2778937ca5c7da2ad6e4bc43fee08
tbourn <thomas.bournaveas@gmail.com>
2019-02-26 22:36:24 +0200

    added animations

--- resources/js/shared.js
+++ resources/js/shared.js
@@ -4,18 +4,25 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var btn = document.querySelector(".main-nav__item-btn");
 
 function closeModal() {
   if (modal) {
     modal.classList.remove("open");
   }
   backdrop.classList.remove("open");
+  setTimeout(function() {
+    backdrop.style.display = "none";
+  }, 200);
 }
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function() {
     modal.classList.add("open");
-    backdrop.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function() {
+      backdrop.classList.add("open");
+    }, 10);
   });
 }
 
@@ -30,7 +37,22 @@ if (modalNoButton) {
 
 toggleButton.addEventListener("click", function() {
     mobileNav.classList.add("open");
-    backdrop.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(function() {
+      backdrop.classList.add("open");
+    }, 10);
+});
+
+btn.addEventListener("animationstart", function(event) {
+  console.log("Animation started", event);
+});
+
+btn.addEventListener("animationend", function(event) {
+  console.log("Animation ended", event);
+});
+
+btn.addEventListener("animationiteration", function(event) {
+  console.log("Animation iteration", event);
 });
 
 $(document).ready(function () {
